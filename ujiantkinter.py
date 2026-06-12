7b678e99b146ef1fb713666d99ad3962f88c0920
argadipta <126493754+argadipta@users.noreply.github.com>
2026-06-12 11:20:31 +0700

    ujiantkinter.py

--- ujiantkinter.py
+++ ujiantkinter.py
@@ -1,101 +1,90 @@
-import tkinter as tk
+soal nomor 1
 from tkinter import ttk, messagebox
 
-#Membuat tampilan aplikasi Tkinter
-soal nomor 1
+soal nomor 2
+root.title("Students Form Application AW3")
 
-#Membuat nama aplikasi 
-soal nomor 2("Students Application AW3")
-
-#Membuat tulisan
-tk.Label(root, text="Hello Students AW3!").grid(row=0, column=0)
-tk.Label(root, text="Please Input Your Identity!").grid(row=1, column=0)
-
-#Membuat tulisan input text
-tk.Label(root, text="First Name ").grid(row=2, column=0)
-tk.Label(root, text="Last Name ").grid(row=3, column=0)
-entry1 = tk.Entry(root)
-entry2 = tk.Entry(root)
 soal nomor 3
-entry2.grid(row=3, column=1)
+tk.Label(root, text="Please input your identity").grid(row=1, column=0)
 
-#Membuat Input Email
+#Input Nama
+tk.Label(root, text="First Name").grid(row=2, column=0)
+soal nomor 4 
+entry_firstname.grid(row=2, column=1)
+
+tk.Label(root, text="Last Name").grid(row=3, column=0)
+entry_lastname = tk.Entry(root)
+entry_lastname.grid(row=3, column=1)
+
+#Input Email
 tk.Label(root, text="Email").grid(row=4, column=0)
-entry_email = tk.Entry(root)
-entry_email.grid(row=4, column=1 )
+soal nomor 5 
+entry_email.grid(row=4, column=1)
 
-#Membuat Input Password
+#Input Password
 tk.Label(root, text="Password").grid(row=5, column=0)
-soal nomor 4
+soal nomor 6
 entry_password.grid(row=5, column=1)
 
-#Membuat Tempat Tanggal lahir
+#Input Tempat Tanggal Lahir
 tk.Label(root, text="Place and Date of Birth").grid(row=6, column=0)
 entry_place = tk.Entry(root)
-entry_place.grid(row=6, column=1)
-frame_ttl = tk.Frame(root)
-frame_ttl.grid(row=7, column=1)
+soal nomor 7
+frame_bod = tk.Frame(root)
+frame_bod.grid(row=7, column=1)
 
-soal nomor 5
+#Tanggal
+soal nomor 8
 date.grid(row=0, column=0)
 date.set("1")
 
-month = ttk.Combobox(frame_ttl, values=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],width=5, state="readonly")
+#Bulan
+month = ttk.Combobox(frame_bod, values=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"], width=5, state="readonly")
 month.grid(row=0, column=1)
-soal nomor 6
+month.set("Jan")
 
-year = ttk.Combobox(frame_ttl, values=list(range(1990, 2025)), width=7, state="readonly")
+#Year
+soal nomor 9
 year.grid(row=0, column=2)
-year.set("2000")
+year.set("1990")
 
-#Input Nomor telepon
-tk.Label(root, text="Phone Number").grid(row=8, column=0)
+#membuat Jenis Kelamin dengan radiobutton
+tk.Label(root, text="Choose Your Gender!").grid(row=8, column=0)
+gender = tk.IntVar()
+soal nomor 10 
+soal nomor 11
+
+#Nomor telepon
+tk.Label(root, text="Phone Number").grid(row=10, column=0)
 entry_phone = tk.Entry(root)
-entry_phone.grid(row=8, column=1)
+entry_phone.grid(row=10, column=1)
 
-#Input Alamat Rumah
-tk.Label(root, text="Address").grid(row=9, column=0)
+#Alamat Rumah
+tk.Label(root, text="Address").grid(row=11, column=0)
 entry_address = tk.Entry(root)
-entry_address.grid(row=9, column=1)
+entry_address.grid(row=11, column=1)
 
-#Membuat radio button (memilih hanya 1 option) jenis kelamin
-tk.Label(root, text="Choose Your Gender!").grid(row=10, column=0)
-gender = tk.IntVar() #untuk membuat option
-tk.Radiobutton(root, text="Male", variable=gender, value=0).grid(row=10, column=1, sticky="w")
-soal nomor 7
-
-#Membuat Combo box kelas
+#Membuat kelas dengan combo box
 tk.Label(root, text="Choose Your Class").grid(row=12, column=0)
-soal nomor 8
+combo_box = ttk.Combobox(root, values=["7AE1", "7AE2", "7AE3", "8AE1", "8AE2", "8AE3"], state="readonly")
 combo_box.grid(row=12, column=1, sticky="w")
-combo_box.set("7 AE 1")
+combo_box.set("7AE1")
 
-#Membuat Check Box Button Makanan kesukaan
+#Membuat makanan kesukaan dengan check button
 tk.Label(root, text="Choose Your Favorite Food").grid(row=13, column=0)
 food1 = tk.IntVar()
-tk.Checkbutton(root, text="Nasi Goreng", variable=food1).grid(row=14, column=1,sticky="w")
+tk.Checkbutton(root, text="Nasi Padang", variable=food1).grid(row=13, column=1, sticky="w")
 food2 = tk.IntVar()
-soal nomor 9
+soal nomor 12
 food3 = tk.IntVar()
-tk.Checkbutton(root, text="Mie Ayam", variable=food3).grid(row=16, column=1,sticky="w")
+tk.Checkbutton(root, text="Bakso", variable=food3).grid(row=15, column=1, sticky="w")
+soal nomor 13
+tk.Checkbutton(root, text="Sate", variable=food4).grid(row=16, column=1, sticky="w")
 
-#Membuat Listbox ukuran sepatu
-tk.Label(root, text="Choose Your Size Shoes").grid(row=17, column=0)
-frame_list = tk.Frame(root)
-frame_list.grid(row=17, column=1, columnspan=2, pady=10)
-scrollbar =tk.Scrollbar(frame_list)
-scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-mylist = tk.Listbox(frame_list, yscrollcommand=scrollbar.set, height=5)
-mylist.pack(side=tk.LEFT, fill=tk.BOTH)
-for line in range (36,45):
-    mylist.insert(tk.END, str(line))
-scrollbar.config(command=mylist.yview)
-
-#Function Message Box Register
-soal nomor 10
-    #First Name and Last Name
-    first_name = entry1.get()
-    last_name = entry2.get()
+#Function Tombol Register
+soal nomor 14
+    first_name = entry_firstname.get()
+    last_name = entry_lastname.get()
     email = entry_email.get()
     password = entry_password.get()
     phone = entry_phone.get()
@@ -103,78 +92,69 @@ soal nomor 10
     
     #Validasi email dan password
     if email == "" or password == "":
-        messagebox.showwarning("Warning", "Email and Password Must be filled")
-    soal nomor 11
-        messagebox.showerror("Error", "Email Invalid")
-    else:
-        messagebox.showinfo("Success", "Registration Success!")
+        soal nomor 15
+    elif "@" not in email:
+        messagebox.showerror("Error!", "Email Invalid!")
+    soal nomor 16
+        messagebox.showinfo("Success!", "The data has been successfully saved")
         
     #Gender
-    if gender.get() == 0:
+    soal nomor 17
         gender_text = "Male"
-    soal nomor 12
+    elif gender.get() == 2:
         gender_text = "Female"
     else:
         gender_text = "Not Selected"
+        
+    #nomor telepon
+    phone = entry_phone.get()
+    if not phone.isdigit():
+        messagebox.showerror("Error", "Phone must be filled by numbers!")
+        soal nomor 18
     
     #Class
     classes = combo_box.get()
     
-    #Phone Number
-    phone = entry_phone.get()
-    soal nomor 13
-        messagebox.showerror("Error", "Phone must filled by numbers!")
-    
-    #Tempat tanggal lahir
-    soal nomor 14
+    #Tanggal Lahir
+    place = entry_place.get()
     d = date.get()
-    m = month.get()
+    soal nomor 19
     y = year.get()
     
-    
-    # Food
+    #Food check button
     foods = []
     if food1.get() == 1:
-        foods.append("Nasi Goreng")
+        foods.append("Nasi Padang")
     if food2.get() == 1:
-        foods.append("Bakso")
+        foods.append("Nasi Goreng")
     if food3.get() == 1:
-        foods.append("Mie Ayam")
+        foods.append("Bakso")
+    if food4.get() == 1:
+        foods.append("Sate")
     if not foods:
         foods_text = "None"
     else:
         foods_text = ", ".join(foods)
-
-    #Shoes
-    try:
-        size = mylist.get(mylist.curselection())
-    except:
-        size = "Not Selected"
         
-    #Summary
+    #SUMMARY DATA INPUT
     summary = f"""
-    ==== STUDENT DATA ====
-    Name    : {first_name} {last_name}
-    Email   : {email}
-    Place   : {place}
-    Date of Birth : {d} {m} {y}
-    Phone   : {soal nomor 15}
-    Address : {address}
-    Gender  : {gender_text}
-    Class   : {soal nomor 16}
-    Food    : {foods_text}
-    Shoe Size : {size}
+    ===== STUDENT DATA =====
+    {soal nomor 20}
+    Email       : {email}
+    Born Date   : {place}, {d} {m} {y}
+    Phone       : {phone}
+    Address     : {address}
+    Gender      : {gender_text}
+    Class       : {classes}
+    Food        : {foods_text}
     """
-    soal nomor 17
+    soal nomor 21
     
+#Tombol
+soal nomor 22
+soal nomor 23
 
-#Membuat Tombol Register
-soal nomor 18
-btn_register.grid(row=20, column=0)
+soal nomor 24
+btn_exit.grid(row=30, column=1, padx=5, pady=10)
 
-#Membuat tombol exit
-soal nomor 19
-btn_exit.grid(row=20, column=1)
-
-#Menampilkan aplikasi Tkinter
-soal nomor 20
+soal nomor 25
